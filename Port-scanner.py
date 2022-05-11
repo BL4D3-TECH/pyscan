2f7e5ba3c9993a869eebf2c964add46f36a98979
Blade <100950599+BL4D3-PHANTOM@users.noreply.github.com>
2022-05-11 23:23:07 +0200

    Update Port-scanner.py

--- Port-scanner.py
+++ Port-scanner.py
@@ -5,17 +5,17 @@ print('''
 BY AL2AL2624
 ''')
 
-ip_objetivo = socket.gethostbyname(input("Introduzca la IP objetivo : "))
+ip_target = socket.gethostbyname(input("Introduzca la IP objetivo : "))
 
-print("Escaneando --> "+ ip_objetivo)
+print("Scanning --> "+ ip_objetivo)
 
 try:
     for ports in range(1,65535):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(0.5)
-        resultado = s.connect_ex((ip_objetivo, ports))
+        resultado = s.connect_ex((ip_target, ports))
         if resultado == 0:
-            print("El puerto {} esta abierto".format(ports))
+            print("the port {} is open".format(ports))
         s.close()
 except:
     print("\n Saliendo...")
