e860f131c80156bdeef199045a89b7e1934301b7
al2al2624 <berto.av209@gmail.com>
2022-05-29 22:20:53 +0200

    first version

--- pyscan-1.0.py
+++ pyscan-1.0.py
@@ -2,21 +2,19 @@ import sys
 import socket
 
 print('''
-BY AL2AL2624
+BY BL4D3
 ''')
 
-ip_target = socket.gethostbyname(input("Introduzca la IP objetivo : "))
-
-print("Scanning --> "+ ip_objetivo)
-
+ip_target = socket.gethostbyname(input("Which IP you will scan: "))
+print("Scanning --> "+ ip_target)
 try:
     for ports in range(1,65535):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(0.5)
+        socket.setdefaulttimeout(0.1)
         resultado = s.connect_ex((ip_target, ports))
         if resultado == 0:
             print("the port {} is open".format(ports))
         s.close()
 except:
-    print("\n Saliendo...")
+    print("\n going down :(")
     sys.exit(0)
